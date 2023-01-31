75321bac6fa595a6a5d342a1e12044a380a343e1
KarthikRdec02 <kingskarthik67@gmail.com>
2023-01-31 11:33:59 +0530

    Project Company Details

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,22 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		// TODO Auto-generated method stub
+System.out.println("NAME : ABC pvt ltd");
+	}
+	private void companyId() {
+		// TODO Auto-generated method stub
+System.out.println("ID : 123456789");
+	}
+	private void companyAddress() {
+		// TODO Auto-generated method stub
+System.out.println("ADDRESS : Tambaram, Chennai");
+	}
+	public static void main(String[] args) {
+		CompanyInfo a=new CompanyInfo();
+		a.companyName();
+		a.companyId();
+		a.companyAddress();  
+	}
+}
